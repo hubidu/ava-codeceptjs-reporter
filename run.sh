0bc22dd89c351c8041d9709f5eb26d7d66a95f71
Stefan Huber <hubidu@users.noreply.github.com>
2017-09-08 14:15:28 +0200

    Change port

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker build -t e2e-reporter . || exit 1
-docker run  -p 3000:3000 -v /opt/e2e/out:/opt/e2e/__out -it e2e-reporter || exit 1
+docker run  -p 4000:4000 -v /opt/e2e/out:/opt/e2e/__out -it e2e-reporter || exit 1
--- services/get-test-runs.js
+++ services/get-test-runs.js
@@ -2,7 +2,7 @@ import 'isomorphic-fetch'
 
 export default async () => {
     // eslint-disable-next-line no-undef
-    const res = await fetch('http://localhost:3000/api/test-runs')
+    const res = await fetch('http://localhost:4000/api/test-runs')
     const json = await res.json()
     return json    
 }
