edfd7fafee0cc45a393f46b4c16f49fb41574377
Stefan Huber <hubidu@users.noreply.github.com>
2017-09-08 14:52:07 +0200

    run as daemon

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker build -t e2e-reporter . || exit 1
-docker run  -p 4000:4000 -v /opt/e2e/out:/opt/e2e/__out -it e2e-reporter || exit 1
+docker run -d -p 4000:4000 -v /opt/e2e/out:/opt/e2e/__out -it e2e-reporter || exit 1
